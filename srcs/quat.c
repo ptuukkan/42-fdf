a9da26ff140fa83587de2e7530c65b5f480a7474
Pekka Tuukkanen <petuukka@gmail.com>
2020-01-11 16:20:04 +0200

    working on quat roation

--- srcs/quat.c
+++ srcs/quat.c
@@ -12,43 +12,53 @@
 
 #include "fdf.h"
 
-t_quat	*rot_to_quat(t_quat *q, float az, float ay, float ax)
+void	quat_normalize(t_quat *q)
+{
+	double	mag;
+
+	mag = sqrt(q->w * q->w + q->x * q->x + q->y * q->y + q->z * q->z);
+	q->w /= mag;
+	q->x /= mag;
+	q->y /= mag;
+	q->z /= mag;
+}
+
+void	rot_to_quat(t_quat *q, float az, float ay, float ax)
 {
 	float	cy;
 	float	sy;
 	float	cp;
 	float	sp;
 	float	cr;
+	float	sr;
 
-	ax = ax * (3.14 / 180);
-	ay = ay * (3.14 / 180);
-	az = az * (3.14 / 180);
-	cy = cos(az / 2);
-	sy = sin(az / 2);
-	cp = cos(ay / 2);
-	sp = sin(ay / 2);
-	cr = cos(ax / 2);
-	q->w = cy * cp * cr + sy * sp * sin(ax / 2);
-	q->x = cy * cp * sin(ax / 2) - sy * sp * cr;
-	q->y = sy * cp * sin(ax / 2) + cy * sp * cr;
-	q->z = sy * cp * cr - cy * sp * sin(ax / 2);
+	ax = ax * (3.14f / 180.0f);
+	ay = ay * (3.14f / 180.0f);
+	az = az * (3.14f / 180.0f);
+	cy = cos(az / 2.0f);
+	sy = sin(az / 2.0f);
+	cp = cos(ay / 2.0f);
+	sp = sin(ay / 2.0f);
+	cr = cos(ax / 2.0f);
+	sr = sin(ax / 2.0f);
+	q->w = cy * cp * cr + sy * sp * sr;
+	q->x = cy * cp * sr - sy * sp * cr;
+	q->y = sy * cp * sr + cy * sp * cr;
+	q->z = sy * cp * cr - cy * sp * sr;
+	quat_normalize(q);
 }
 
-void	quat_multiply(t_quat *q1, t_quat *q2)
+t_quat	*quat_multiply(t_quat *q1, t_quat *q2)
 {
-	float	w;
-	float	x;
-	float	y;
-	float	z;
+	t_quat	*q;
 
-	w = q1->w * q2->w - q1->x * q2->x - q1->y * q2->y - q1->z * q2->z;
-	x = q1->w * q2->x + q1->x * q2->w + q1->y * q2->z - q1->z * q2->y;
-	y = q1->w * q2->y + q1->y * q2->w + q1->z * q2->x - q2->x * q2->z;
-	z = q1->w * q2->z + q1->z * q2->w + q1->x * q2->y - q1->y * q2->x;
-	q1->w = w;
-	q1->x = x;
-	q1->y = y;
-	q1->z = z;
+	if (!(q = (t_quat *)ft_memalloc(sizeof(t_quat))))
+		ft_exiterror("error", 3, 2);
+	q->w = q1->w * q2->w - q1->x * q2->x - q1->y * q2->y - q1->z * q2->z;
+	q->x = q1->w * q2->x + q1->x * q2->w + q1->y * q2->z - q1->z * q2->y;
+	q->y = q1->w * q2->y + q1->y * q2->w + q1->z * q2->x - q2->x * q2->z;
+	q->z = q1->w * q2->z + q1->z * q2->w + q1->x * q2->y - q1->y * q2->x;
+	return (q);
 }
 
 void	quat_conjugate(t_quat *q)
@@ -91,10 +101,10 @@ void	quat_rotate(t_fdf *fdf)
 	rot_to_quat(q, fdf->line.z_angle, fdf->line.y_angle, fdf->line.x_angle);
 	vec_to_quat(p0, fdf->line.x0, fdf->line.y0, fdf->line.z0);
 	vec_to_quat(p1, fdf->line.x1, fdf->line.y1, fdf->line.z1);
-	quat_multiply(p0, q);
-	quat_multiply(p1, q);
+	p0 = quat_multiply(q, p0);
+	p1 = quat_multiply(q, p1);
 	quat_conjugate(q);
-	quat_multiply(p0, q);
-	quat_multiply(p1, q);
+	p0 = quat_multiply(p0, q);
+	p1 = quat_multiply(p1, q);
 	quat_to_vec(p0, p1, fdf);
 }
